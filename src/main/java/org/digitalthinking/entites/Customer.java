bca9a62681a2bb47ae5673518d82c09650578c75
Joseph Magallanes <josephcarlos.jcmn@gmail.com>
2025-07-12 10:05:48 -0500

    first commit

---
+++ src/main/java/org/digitalthinking/entites/Customer.java
@@ -0,0 +1,26 @@
+package org.digitalthinking.entites;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Data
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String code;
+    private String accountNumber;
+    private String names;
+    private String surname;
+    private String phone;
+    private String address;
+    @OneToMany(mappedBy = "customer",cascade = {CascadeType.ALL},fetch = FetchType.EAGER)
+    @JsonManagedReference
+    private List<Product> products;
+
+}
---
+++ src/main/java/org/digitalthinking/repositories/CustomerRepository.java
@@ -0,0 +1,41 @@
+package org.digitalthinking.repositories;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import org.digitalthinking.entites.Customer;
+
+import java.util.List;
+
+@ApplicationScoped
+public class CustomerRepository {
+    @Inject
+    EntityManager em;
+
+    @Transactional
+    public void createdCustomer(Customer p) {
+        em.persist(p);
+    }
+
+    @Transactional
+    public void deleteCustomer(Customer p) {
+        em.remove(em.contains(p) ? p : em.merge(p));
+    }
+
+    @Transactional
+    public List<Customer> listCustomer() {
+        List<Customer> customers = em.createQuery("select p from Customer p").getResultList();
+        return customers;
+    }
+
+    @Transactional
+    public Customer findCustomer(Long Id) {
+        return em.find(Customer.class, Id);
+    }
+
+    @Transactional
+    public void updateCustomer(Customer p) {
+        em.merge(p);
+    }
+}
